13e208d1321d6d0b4eee0a65d46070b51bf853ec
Caio Pelozzi <caiopelozzi18@gmail.com>
2025-09-16 21:01:56 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Caio", "caiopelozzi@gamil.com", "99999", "123321");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
